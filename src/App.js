07e529776bd5af123759975d16fcdaf9e1ca6570
noob-sumit <60885323+noob-sumit@users.noreply.github.com>
2022-08-28 23:09:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router >
         <Header />
         <Routes>
-          <Route path ="/" element={<Home />}></Route>
+          <Route path ="/" exact element={<Home />}></Route>
           <Route path ="movie/:id" element = {<Movie />}></Route>
           <Route path ="movies/:type" element = {<MovieList />}></Route>
           <Route path ="/*" element = {<h1>error page</h1>}></Route>
