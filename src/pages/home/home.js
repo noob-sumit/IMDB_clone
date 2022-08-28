f6a4b599420b853bd46b28d946d078d4dc81e060
noob-sumit <ss287128@gmail.com>
2022-08-28 16:44:57 +0530

    deploying resolved

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./home.css"
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from "react-router-dom";
-import MovieList from "../../movieList/movieList";
+import MovieList from "../../components/movieList/movieList";
 
 const Home = () => {
     const [popularMovies , setPopularMovies] = useState([])
